3dad6060bf86da36e7df5597d649478c645c51e9
columbaSilva <2236001173@utna.edu.mx>
2025-06-20 17:02:46 -0600

    se realisa la coneccion a la base de datos mongodb

---
+++ app/config/conexion.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const config = require('./configuracion')
+
+module.exports = {
+    connection: null,
+    connect: () => {
+        if (this.connection) return this.connection
+        return mongoose.connect(config.DB)
+            .then(conn => {
+                this.connection = conn
+                console.log('la conexión se realizó de manera correcta');
+            })
+            .catch(e => { console.log('error en la conexión', e) })
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app= require('./app/app')
 const config= require('./app/config/configuracion')
-
+const conexion = require('./app/config/conexion')
 
 app.listen(config.PORT, ()=>{
     console.log(`Servidor corriendo en el puerto:`, config.PORT);
